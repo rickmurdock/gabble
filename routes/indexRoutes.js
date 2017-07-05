0c14f8479a255e0dcd512690705c7bd902793697
rickmurdock <rick.murdock@live.com>
2017-07-04 23:26:49 -0500

    Initial display of gabs on homepage.

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -4,7 +4,18 @@ const shared = require('../public/sharedFunctions.js');
 const models = require("../models");
 
 indexRouter.get("/", shared.checkAuth, function(req, res) {
-  res.render("index", { user: req.session.user });
+  // res.render("index", { user: req.session.user });
+  console.log("IN INDEX ROUTER>>>>>>>>>>>>>>>");
+  models.message
+    .findAll()
+    .then(function(foundMessages) {
+      console.log("FOUND______", foundMessages);
+    res.render("index", { messages: foundMessages,
+                          user: req.session.user });
+  })
+  .catch(function(err) {
+    res.status(500).send(err);
+  }); 
 });
 
 module.exports = indexRouter;
