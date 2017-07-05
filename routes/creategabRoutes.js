1b61063a237e9bb3ba51ac1d9bfc7dbe5e24bc83
rickmurdock <rick.murdock@live.com>
2017-07-04 22:56:51 -0500

    Created post method for adding a gab.

--- routes/creategabRoutes.js
+++ routes/creategabRoutes.js
@@ -7,4 +7,23 @@ creategabRouter.get("/", shared.checkAuth, function(req, res) {
   res.render('creategab', { user : req.session.user});
 });
 
+creategabRouter.post("/", function(req, res) {
+  if(!req.body || !req.body.newgab) {
+    res.redirect("/creategab");
+  }
+
+  var newGab = models.message.build({
+    gab : req.body.newgab,
+    authorId : 1
+  });
+  newGab
+    .save()
+    .then(function(savedGab) {
+      res.redirect("/");
+    })
+    .catch(function(err) {
+      res.status(500).send(err);
+    });
+});
+
 module.exports = creategabRouter;
