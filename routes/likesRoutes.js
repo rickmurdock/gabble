78be47f9d8a2b89e2bb8438f1a50838f746441ac
rickmurdock <rick.murdock@live.com>
2017-07-09 16:52:24 -0500

    Like page initial functionality working.

--- routes/likesRoutes.js
+++ routes/likesRoutes.js
@@ -1,8 +1,57 @@
-var express  = require('express');
-var likesRouter = express.Router();
+const express  = require('express');
+const likesRouter = express.Router();
+const models = require("../models");
 
-likesRouter.get("/", function(req, res) {
-  res.render('likes');
+likesRouter.get("/:id", function(req, res) {
+  models.message
+    .findOne({ 
+      where: { id: req.params.id },
+      include: [
+        {
+          model: models.user,
+          as: "author"
+        },
+        {
+          model: models.like,
+          as: "likes",
+          include: { 
+            model: models.user,
+            as: "user"
+          }
+        }
+      ]
+    })
+
+    // .findOne({ 
+    //   where: {
+    //     id: req.params.id 
+    //   },
+    //   inclued: [
+    //     {
+    //       model: models.user,
+    //       as: "author"
+    //     },
+    //     {
+    //       model: models.like,
+    //       as: "likes",
+    //       include: { 
+    //         model: models.user,
+    //         as: "user"
+    //       }
+    //     }
+    //   ]
+    // })
+
+  .then(function(foundLikes) {
+    console.log(foundLikes);
+    res.render("likes", { messages: foundLikes,
+                          user: req.session.user });
+  })
+  .catch(function(err) {
+    res.status(500).send(err);
+  }); 
+  
+  // res.render('likes');
 });
 
 module.exports = likesRouter;
